c9cbd7ce5eca64422f72baef007b5d77b3d19425
Zena0128 <0128ja@gmail.com>
2024-10-30 18:14:17 +0900

    10/31

---
+++ junga/241031/b11866.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class b11866 {
+    private static int k;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        k = Integer.parseInt(input[1]);
+        
+        Circle list = new Circle();
+        for (int i=1;i<=n;i++) {
+            list.add(i);
+        }
+
+        bw.write("<");
+        list.deleteKthNode(k, bw);
+        bw.write(">");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static class Circle {
+        private Node head;
+        private Node tail;
+        private Node current; // 현재 위치한 노드 정보
+
+        private class Node {
+            int data;
+            Node next;
+
+            Node(int data) {
+                this.data = data;
+            }
+        }
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                tail = newNode;
+                tail.next = head;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+
+        // 1번째 노드부터 시작해 k번째 노드들 출력/삭제해가며 조회
+        public void deleteKthNode(int k, BufferedWriter bw) throws IOException {
+            if (isEmpty() || k <= 0) return;
+
+            current = head;
+            Node prev = tail;
+
+            while (head != tail) {
+                for (int i=1;i<k;i++) { // k번째 뒤 노드로 이동
+                    prev = current;
+                    current = current.next;
+                }
+                bw.write(current.data + ", ");
+                // 현재 노드가 head, tail, 그 외인지에 따라 노드 삭제
+                if (current == head) {
+                    head = head.next;
+                    tail.next = head;
+                } else if (current == tail) {
+                    tail = prev;
+                    tail.next = head;
+                } else {
+                    prev.next = current.next;
+                }
+                current = current.next;
+            }
+            bw.write(current.data + "");
+        }
+    }
+}
---
+++ junga/241031/b1929.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class b1929 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        sc.close();
+
+        int[] arr = new int[1000001];
+        for (int i=2;i<=n;i++) {
+            arr[i] = i;
+        }
+
+        for (int i=2;i<=n;i++) {
+            if (arr[i] == 0) continue;
+            
+            for (int j=i*2;j<=n;j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i=m;i<=n;i++) {
+            if (arr[i] != 0) bw.write(arr[i] + "\n"); 
+        }
+        bw.flush();
+    }
+}
