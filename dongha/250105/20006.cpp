57538bc3b32c44c533e1f467cf50702a1b4a6af7
movingsummer <movingsummer0x16@gmail.com>
2025-01-06 18:12:05 +0900

    250105 study

--- dongha/250105/20006.cpp
+++ dongha/250105/20006.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
 
+/*
+각 방마다 처음 입장한 사람의 레벨, 이름 목록 보관
+들어갈 수 있는 방 찾아서 있으면 넣고 없으면 방 새로 만들어줌
+*/
 std::map<std::string, int> nl;
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
--- dongha/250105/2232.cpp
+++ dongha/250105/2232.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 
-std::map<std::string, int> nl;
+/*
+매 순간 남아있는 지뢰 중에서 가장 파워가 큰 지뢰를 폭발시키는 게 최솟값을 찾는 방법이다.
+가장 파워가 큰 지뢰 말고 다른 지뢰를 폭발시켰는데 그게 최솟값이라고 하자.
+4 5 1 과 같이 있을 때 4를 먼저 터뜨리고 5를 터뜨리면 2번인데 실제 최솟값은 5를 터뜨렸을 때의 1번이므로 모순이다.
+따라서 가장 큰 지뢰를 폭발시켰을 때가 최솟값인 걸 증명할 수 있다.
+(지뢰 파워, 지뢰 번호)를 배열에 담고 파워가 큰 순으로 정렬해서 아직 터지지 않은 지뢰면 터뜨린다.
+지뢰를 터뜨리는 것은 재귀로 구현했다.'
+시간복잡도는 O(NlgN)이다.
+*/
+int n;
+void bomb(int i, std::vector<int>& a, std::vector<bool>& check) {
+  check[i] = true;
+  if (i > 0 && a[i - 1] < a[i] && !check[i - 1]) bomb(i - 1, a, check);
+  if (i < n - 1 && a[i + 1] < a[i] && !check[i + 1]) bomb(i + 1, a, check);
+}
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  int n, m;
-  std::cin >> n >> m;
-  std::vector<int> r;
-  std::vector rp(0, std::vector<std::string>());
+  std::cin >> n;
+  std::vector<int> a(n);
+  std::vector<std::pair<int, int>> o(n);
+  std::vector<bool> check(n);
   for (int i = 0; i < n; ++i) {
-    std::string name;
-    int l;
-    std::cin >> l >> name;
-    nl[name] = l;
+    std::cin >> a[i];
+    o[i] = {-a[i], i};
+  }
+  sort(o.begin(), o.end());
+  std::vector<int> ans;
 
-    int rn = -1;
-    for (int i = 0; i < r.size(); ++i) {
-      if (r[i] - 10 <= l && l <= r[i] + 10 && rp[i].size() < m) {
-        rn = i;
-        break;
-      }
-    }
-
-    if (rn == -1) {
-      r.push_back(l);
-      std::vector<std::string> nn = {name};
-      rp.push_back(nn);
-    } else {
-      rp[rn].push_back(name);
-    }
+  for (int i = 0; i < n; ++i) {
+    auto [_, idx] = o[i];
+    if (check[idx]) continue;
+    ans.push_back(idx + 1);
+    bomb(idx, a, check);
   }
 
-  for (int i = 0; i < r.size(); ++i) {
-    if (rp[i].size() == m)
-      std::cout << "Started!\n";
-    else
-      std::cout << "Waiting!\n";
-    std::sort(rp[i].begin(), rp[i].end());
-    for (auto n : rp[i]) {
-      std::cout << nl[n] << " " << n << "\n";
-    }
-  }
+  std::sort(ans.begin(), ans.end());
+  for (auto i : ans) std::cout << i << "\n";
 }
